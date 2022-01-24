fdfcaaa5f1d096de252aac9d1c7006e94b149e28
ajiways <ajiwayswc@gmail.com>
2022-01-24 23:24:48 +0700

    метод создания корзины

--- src/services/carts.service.ts
+++ src/services/carts.service.ts
@@ -1,21 +1,56 @@
 import { Cart } from "../entities/cart.entity";
 import { Connection, Repository } from "typeorm";
+import { CustomExcteption } from "../exceptions/custom.exception";
+import { OrdersService } from "./orders.service";
+import { orderStatus } from "../entities/order.entity";
+import { ProductsService } from "./products.service";
 
 export class CartsService {
    private readonly cartsRepository: Repository<Cart>;
+   private readonly orderService: OrdersService;
+   private readonly productsService: ProductsService;
 
-   constructor(dataProvider: Connection) {
+   constructor(dataProvider: Connection, orderService: OrdersService, productsService: ProductsService) {
       this.cartsRepository = dataProvider.getRepository(Cart);
+      this.orderService = orderService;
+      this.productsService = productsService;
+   }
+
+   async createCart(customer_id: number, product_id: number, amount: number): Promise<boolean> {
+      const order = await this.orderService.createOrder(customer_id);
+      console.log(`Order: ${order}`);
+      const product = await this.productsService.findOne(product_id);
+      console.log(`Product ${product}`);
+
+      if (!order) {
+         throw CustomExcteption.NotFound("Заказ не найден");
+      } else if (!product) {
+         throw CustomExcteption.NotFound("Товар не найден");
+      }
+
+      const cart = this.cartsRepository.create({
+         order: order,
+         product: product,
+         amount: amount,
+      });
+
+      await cart.save();
+
+      return true;
    }
 
    async findAll(): Promise<Cart[]> {
-      return this.cartsRepository.find();
+      const result = this.cartsRepository.find();
+      if (!result) {
+         throw CustomExcteption.NotFound("Empty querry result!");
+      }
+      return result;
    }
 
    async findOne(id: number): Promise<Cart> {
       const result = await this.cartsRepository.findOne(id);
       if (!result) {
-         throw new Error("Empty querry result!");
+         throw CustomExcteption.NotFound("Empty querry result!");
       }
       return result;
    }
