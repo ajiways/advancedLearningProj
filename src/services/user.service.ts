a141d43518de4de0e76d33d625a4fd602ea3d5d0
ajiways <ajiwayswc@gmail.com>
2022-01-24 23:23:06 +0700

    контроллер и сервис для юзеров

---
+++ src/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Connection, Repository } from "typeorm";
+import { User } from "../entities/user.entity";
+import { CustomExcteption } from "../exceptions/custom.exception";
+
+export class UsersService {
+   private readonly usersRepository: Repository<User>;
+
+   constructor(dataProvider: Connection) {
+      this.usersRepository = dataProvider.getRepository(User);
+   }
+
+   async findAll(): Promise<User[]> {
+      const result = await this.usersRepository.find();
+      if (!result) {
+         throw CustomExcteption.NotFound("Empty querry result!");
+      }
+      return result;
+   }
+
+   async findOne(id: number): Promise<User> {
+      const result = await this.usersRepository.findOne({ where: { id: id } });
+      console.log(`User: ${result}`);
+      if (!result) {
+         throw CustomExcteption.NotFound("Empty querry result!");
+      }
+      return result;
+   }
+}
