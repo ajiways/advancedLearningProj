f3e53ee31b44bc1cf4abf080fd2c32a1c4692675
ajiways <ajiwayswc@gmail.com>
2022-01-24 23:23:40 +0700

    создание корзины и переделка роутера для неё

--- src/controllers/carts.controller.ts
+++ src/controllers/carts.controller.ts
@@ -1,6 +1,8 @@
 import { Cart } from "../entities/cart.entity";
 import { CartsService } from "../services/carts.service";
 import { RequestInterface } from "../infterfaces/request.interface";
+import { CustomExcteption } from "../exceptions/custom.exception";
+import express from "express";
 
 export class CartsController {
    private readonly cartsService: CartsService;
@@ -9,13 +11,36 @@ export class CartsController {
       this.cartsService = cartsService;
    }
 
+   async createCart(request: RequestInterface, response: express.Response): Promise<string> {
+      if (!request.body.product_id || !request.body.amount || !request.body.customer_id) {
+         throw CustomExcteption.BadRequest("Неправильно заполнены поля в форме");
+      } else if (
+         Number(request.body.customer_id) <= 0 ||
+         Number(request.body.product_id) <= 0 ||
+         Number(request.body.amount) <= 0
+      ) {
+         throw CustomExcteption.BadRequest("Неправильно заполнены поля в форме");
+      }
+
+      const created = await this.cartsService.createCart(
+         Number(request.body.customer_id),
+         Number(request.body.product_id),
+         Number(request.body.amount)
+      );
+      if (created) {
+         response.status(201);
+      }
+
+      return JSON.stringify({ message: "Заказ успешно создан!" });
+   }
+
    async getAllCarts(): Promise<Cart[]> {
       return this.cartsService.findAll();
    }
 
    async getCartById(request: RequestInterface): Promise<Cart> {
       if (!request.body || !request.params.id || !Number(request.params.id)) {
-         throw new Error("Bad request");
+         throw CustomExcteption.BadRequest("No params for this request was provided");
       }
       return this.cartsService.findOne(Number(request.params.id));
    }
--- src/routers/carts.router.ts
+++ src/routers/carts.router.ts
@@ -2,6 +2,18 @@ import { EMethod } from "../infterfaces/server.interface";
 import { CartsService } from "../services/carts.service";
 import { CartsController } from "../controllers/carts.controller";
 import { Server } from "../server";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { ProductsService } from "../services/products.service";
+import { OrdersService } from "../services/orders.service";
+import { UsersService } from "../services/user.service";
+
+/**
+ *
+ * @swagger
+ * tags:
+ *    name: POST
+ *    description: For post requests
+ */
 
 /**
  * @swagger
@@ -14,10 +26,10 @@ import { Server } from "../server";
  *                type: number
  *                description: The auto-generate id of the cart
  *             order_id:
- *                type: number
+ *                type: string
  *                description: id of order that was generated for this cart
  *             amount:
- *                type: number
+ *                type: string
  *                description: number of products in this cart
  *          example:
  *             id: 15
@@ -64,12 +76,77 @@ import { Server } from "../server";
  *                      $ref: '#/components/schemas/Cart'
  */
 
+/**
+ * @swagger
+ * /cart/create:
+ *    post:
+ *       summary: Create new cart with order inside
+ *       tags: [POST]
+ *       requestBody:
+ *          required: true
+ *          content:
+ *             application/json:
+ *                schema:
+ *                   type: object
+ *                   properties:
+ *                      customerId:
+ *                         type: string
+ *                         description: The customer's id for this order
+ *                         example: '10'
+ *                      productId:
+ *                         type: string
+ *                         description: The product's id for this order
+ *                         example: '322'
+ *                      amount:
+ *                         type: string
+ *                         description: Amount of the products to be ordered
+ *                         example: '14'
+ *       responses:
+ *          201:
+ *             description: if cart and order was created successful
+ *             content:
+ *                application/json:
+ *                   schema:
+ *                      type: object
+ *                      properties:
+ *                         id:
+ *                            type: number
+ *                            description: The auto-generate id of the cart
+ *                            example: 15
+ *                         order_id:
+ *                            type: string
+ *                            description: id of order that was generated for this cart
+ *                            example: '322'
+ *                         amount:
+ *                            type: string
+ *                            description: number of products in this cart
+ *                            example: '11'
+ *          403:
+ *             description: if user isn't authorized yet
+ *             content:
+ *                application/json:
+ *                   schema:
+ *                      type: object
+ *                      properties:
+ *                         message:
+ *                            type: string
+ *                            description: description of the error
+ *                            example: "user not authorized"
+ */
+
 export async function cartRouter(serverInstance: Server) {
    const provider = serverInstance.getDBConnection();
-   const cartsService = new CartsService(provider);
+   const productsService = new ProductsService(provider);
+   const userService = new UsersService(provider);
+   const ordersService = new OrdersService(provider, userService);
+   const cartsService = new CartsService(provider, ordersService, productsService);
    const cartsController = new CartsController(cartsService);
 
    serverInstance.addHandler(EMethod.GET, "/carts", cartsController.getAllCarts.bind(cartsController));
 
    serverInstance.addHandler(EMethod.GET, "/carts/:id", cartsController.getCartById.bind(cartsController));
+
+   serverInstance.addHandler(EMethod.POST, "/cart/create", cartsController.createCart.bind(cartsController), [
+      authMiddleware,
+   ]);
 }
