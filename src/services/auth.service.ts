a42a770e25fbee84f676a2b667043982210b1c8d
ajiways <ajiwayswc@gmail.com>
2022-01-27 18:35:26 +0700

    фикс приведения типов

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Repository, Connection } from "typeorm";
 import { User } from "../entities/user.entity";
 import bcrypt from "bcrypt";
 import { CustomException } from "../exceptions/custom.exception";
-import tokensService, { userData } from "./tokens.service";
+import tokensService from "./tokens.service";
 
 export class AuthService {
   private readonly authRepository: Repository<User>;
@@ -12,9 +12,9 @@ export class AuthService {
   }
 
   async login(email: string, password: string): Promise<string> {
-    const candidate = (await this.authRepository.findOne({
+    const candidate = await this.authRepository.findOne({
       where: { email },
-    })) as userData;
+    });
 
     if (!candidate) {
       throw CustomException.BadRequest("Wrong email or password");
--- src/services/tokens.service.ts
+++ src/services/tokens.service.ts
@@ -2,25 +2,23 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { configService } from "./config.service";
 
 export interface userData extends JwtPayload {
-   id: number;
-   firstName: string;
-   password: string;
+  id: number;
+  firstName: string;
+  password: string;
 }
 
 class TokensService {
-   generateToken(payload: Record<string, string>): string {
-      const token = jwt.sign(payload, configService.secret);
-      return token;
-   }
+  generateToken(payload: userData): string {
+    return jwt.sign(payload, configService.secret);
+  }
 
-   validateToken(token: unknown): userData | null {
-      try {
-         const decodedData = jwt.verify(String(token), configService.secret) as userData;
-         return decodedData;
-      } catch (error) {
-         return null;
-      }
-   }
+  validateToken(token: unknown): userData | null {
+    try {
+      return jwt.verify(String(token), configService.secret) as userData;
+    } catch (error) {
+      return null;
+    }
+  }
 }
 
 export default new TokensService();
