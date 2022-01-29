178e6f85458adf85ed9d2337d1834141185699bd
ajiways <ajiwayswc@gmail.com>
2022-01-30 01:36:09 +0700

    микрофиксы

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,48 +6,39 @@ import { isCorrectString } from "../guards/isCorrectString.guard";
 import { responseInterface } from "../infterfaces/response.interface";
 
 export class AuthController {
-  private readonly authService: AuthService;
+   private readonly authService: AuthService;
 
-  constructor(authService: AuthService) {
-    this.authService = authService;
-  }
+   constructor(authService: AuthService) {
+      this.authService = authService;
+   }
 
-  async login(request: RequestInterface): Promise<responseInterface> {
-    if (request.user) {
-      throw CustomException.BadRequest("Already logged in");
-    }
+   async login(request: RequestInterface): Promise<responseInterface> {
+      if (request.user) {
+         throw CustomException.BadRequest("Already logged in");
+      }
 
-    if (
-      !isCorrectString(request.body.email) ||
-      !isCorrectString(request.body.password)
-    ) {
-      throw CustomException.BadRequest("No email or password was provided");
-    }
+      if (!isCorrectString(request.body.email) || !isCorrectString(request.body.password)) {
+         throw CustomException.BadRequest("No email or password was provided");
+      }
 
-    const token = await this.authService.login(
-      request.body.email,
-      request.body.password
-    );
+      const token = await this.authService.login(request.body.email, request.body.password);
 
-    return {
-      message: "Добро пожаловать!",
-      cookie: {
-        token,
-      },
-    };
-  }
+      return {
+         message: "Добро пожаловать!",
+         cookie: {
+            token,
+         },
+      };
+   }
 
-  async logout(
-    request: express.Request,
-    response: express.Response
-  ): Promise<Record<string, string>> {
-    const token = request.cookies.token;
-    if (!token) {
-      throw CustomException.BadRequest("To log out, log in first");
-    }
+   async logout(request: express.Request, response: express.Response): Promise<Record<string, string>> {
+      const token = request.cookies.token;
+      if (!token) {
+         throw CustomException.BadRequest("To log out, log in first");
+      }
 
-    response.clearCookie("token");
-    response.status(200);
-    return { message: "Successfully logged out!" };
-  }
+      response.clearCookie("token");
+      response.status(200);
+      return { message: "Successfully logged out!" };
+   }
 }
--- src/services/tokens.service.ts
+++ src/services/tokens.service.ts
@@ -2,23 +2,23 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { configService } from "./config.service";
 
 export interface userData extends JwtPayload {
-  id: number;
-  firstName: string;
-  password: string;
+   id: number;
+   firstName: string;
+   password: string;
 }
 
 class TokensService {
-  generateToken(payload: userData): string {
-    return jwt.sign({ payload }, configService.secret);
-  }
+   generateToken(payload: userData): string {
+      return jwt.sign({ payload }, configService.secret);
+   }
 
-  validateToken(token: unknown): userData | null {
-    try {
-      return jwt.verify(String(token), configService.secret) as userData;
-    } catch (error) {
-      return null;
-    }
-  }
+   validateToken(token: unknown): userData | null {
+      try {
+         return jwt.verify(String(token), configService.secret) as userData;
+      } catch (error) {
+         return null;
+      }
+   }
 }
 
 export default new TokensService();
